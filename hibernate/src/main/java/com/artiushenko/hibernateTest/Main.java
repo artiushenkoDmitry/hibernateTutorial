041ab8738667839e646898678b269cfad730772f
dmitry.artiushenko <dmitry.artiushenko@gmail.com>
2019-06-13 09:13:46 +0300

    small refactoring

--- hibernate/src/main/java/com/artiushenko/hibernateTest/Main.java
+++ hibernate/src/main/java/com/artiushenko/hibernateTest/Main.java
@@ -1,20 +1,21 @@
 package com.artiushenko.hibernateTest;
 
-import com.artiushenko.hibernateTest.bl.HibernateUtil;
 import com.artiushenko.hibernateTest.entity.Town;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class Main {
     public static void main(String[] args) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
+        SessionFactory sessionFactory =  new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
         session.beginTransaction();
 
         Town town = new Town();
-        town.setId(7);
         town.setTown("newTown2");
 
         session.save(town);
         session.getTransaction().commit();
-        HibernateUtil.shutdown();
+        sessionFactory.close();
     }
 }
